4a218d9e2188399b18d052fc9aa6c1244f2fde22
Jonathan Martinez <57384298+jonathan-martin-dev@users.noreply.github.com>
2019-11-28 13:56:26 -0800

    Add Quotes Array

--- js/script.js
+++ js/script.js
@@ -10,7 +10,38 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+const quotes = [
+  {
+    quoute: 'Execute Order 66.',
+    source: 'Darth Sidious (Senator Sheev Palpatine)',
+    citation: 'Star Wars: Episode III - Revenge of The Sith',
+    year: '19 BBY'
+  },
+  {
+    quoute: 'I don\'t know if you heard but the Empire is gone.',
+    source: 'The Mandolorian',
+    citation: 'Star Wars: The Mandalorian',
+    year: '9 ABY'
+  },
+  {
+    quoute: 'Cal Kestis. How Predictable. Oh yes, I know your name...',
+    source: 'Trilla Suduri (The Second Sister)',
+    citation: 'Star Wars: Jedi Fallen Order',
+    year: '14 BBY'
+  },
+  {
+    quoute: 'The force is what gives a Jedi his power. It’s an energy field created by all living things. It surrounds us and penetrates us. It binds the Galaxy together.',
+    source: 'Obi-Wan Kenobi',
+    citation: 'Star Wars: Episode IV - A New Hope',
+    year: '0 BBY/0 ABY'
+  },
+  {
+    quoute: '[To Darth Vader] I thought I know who you were under that mask. But it\'s impossible. My master could never be so vile.',
+    source: 'Ahsoka Tano',
+    citation: 'Star Wars: Rebels',
+    year: '4 BBY'
+  }
+]
 
 
 /***
