63f94f23f4ecdc87c6cd92a5599dbd44d9dff5d7
Jonathan Martinez <57384298+jonathan-martin-dev@users.noreply.github.com>
2019-11-28 14:10:11 -0800

    Added Tags Key

--- js/script.js
+++ js/script.js
@@ -15,31 +15,36 @@ const quotes = [
     quoute: 'Execute Order 66.',
     source: 'Darth Sidious (Senator Sheev Palpatine)',
     citation: 'Star Wars: Episode III - Revenge of The Sith',
-    year: '19 BBY'
+    year: '19 BBY',
+    tags: ['Sith', 'Clone Wars']
   },
   {
     quoute: 'I don\'t know if you heard but the Empire is gone.',
     source: 'The Mandolorian',
     citation: 'Star Wars: The Mandalorian',
-    year: '9 ABY'
+    year: '9 ABY',
+    tags: ['Mandalore', 'Post-Empire']
   },
   {
     quoute: 'Cal Kestis. How Predictable. Oh yes, I know your name...',
     source: 'Trilla Suduri (The Second Sister)',
     citation: 'Star Wars: Jedi Fallen Order',
-    year: '14 BBY'
+    year: '14 BBY',
+    tags: ['Inquisitor', 'Order 66', 'Empire']
   },
   {
     quoute: 'The force is what gives a Jedi his power. It’s an energy field created by all living things. It surrounds us and penetrates us. It binds the Galaxy together.',
     source: 'Obi-Wan Kenobi',
     citation: 'Star Wars: Episode IV - A New Hope',
-    year: '0 BBY/0 ABY'
+    year: '0 BBY/0 ABY',
+    tags: ['Jedi', 'Force', 'Empire']
   },
   {
     quoute: '[To Darth Vader] I thought I know who you were under that mask. But it\'s impossible. My master could never be so vile.',
     source: 'Ahsoka Tano',
     citation: 'Star Wars: Rebels',
-    year: '4 BBY'
+    year: '4 BBY',
+    tags: ['Jedi', 'Force', 'Pre-Empire']
   }
 ]
 
